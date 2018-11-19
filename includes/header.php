44ff4422316d3af1f15305ef81f95f1802de72a5
gitc84 <schoolgitter@gmail.com>
2018-11-19 12:03:18 +0100

    header

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,14 @@
 <header>
     <div class="headersticky">
         <div class="header">
+            <div class="desktopbuttons">
+                <ul>
+                    <li id="menuutb">Utbildningar</li>
+                    <li id="menustu">Studenttjänster</li>
+                    <li id="menuvag">Vägledning</li>
+                    <a href="kontakt.php"><li id="menukon">Kontakt</li></a>
+                </ul>
+            </div>
             <div class="icons">
                 <ion-icon class="menu" id="menu" name="menu"></ion-icon>
                 <ion-icon class="headericon" id="searchicon" name="search"></ion-icon>
@@ -25,8 +33,53 @@
                 </form>
             </div>
 
+            <div class="desktoputbildningar">
+                <a href="java.php"><p>Java- och webbutvecklare</p></a>
+                <p>IT- och virtualiseringsspecialist</p>
+                <a href="IT.php"><p>IT-projektledare</p></a>
+                <p>Aktiveringspedagog inom LSS</p>
+                <p>Specialiserad studiehandledare</p>
+                <p>Information</p>
+            </div>
+
             <div class="searchfield"><input type="text" placeholder="Sök..."></div>
 
+            <div class="header-nav">
+
+                <p id="utbildningar-click">Utbildningar &#709;</p>
+
+                <div id="utbildningar-dropdown">
+                    <a href="java.php"><p>Java- och webbutvecklare</p></a>
+                    <p>IT- och virtualiseringsspecialist</p>
+                    <p>IT-projektledare</p>
+                    <p>Aktiveringspedagog inom LSS</p>
+                    <p>Specialiserad studiehandledare</p>
+                    <p>Information</p>
+                </div>
+                <p id="studenttjänster-click">Studenttjänster</p>
+
+                <div id="studenttjänster-dropdown">
+                    <p>Intyg, betyg och slutbetyg</p>
+                    <p>Prövning och validering</p>
+                    <p>Studiemedel</p>
+                    <p>Schema</p>
+                    <p>Studentmail</p>
+                    <p>Lärplattform Itslearning</p>
+                </div>
+                <p id="vägledning-click">Studie- och yrkesvägledning</p>
+                <div id="vägledning-dropdown">
+                    <p>För dig som är över 20 år</p>
+                    <p>För dig som är under 20 år</p>
+                    <p>För dig med särskilda behov</p>
+                </div>
+                <p id="kontakt-click">Kontakta oss</p>
+                <div id="kontakt-dropdown">
+                    <p>Ledning</p>
+                    <p>Studie- och yrkesvägledning</p>
+                    <p>Administration</p>
+                    <p>Hitta hit</p>
+                </div>
+            </div>
         </div>
 
     </div>
--- java/js.js
+++ java/js.js
@@ -1,62 +1,181 @@
+// Kod som döljer och visar meny-val
+
 $('.login').hide();
 
 $('#usericon').click(function(){
-    $('.login').slideToggle(500);
+    $('.login').toggle();
 });
 
 $('.searchfield').hide();
 
 $('#searchicon').click(function(){
-    $('.searchfield').slideToggle(500);
+    $('.searchfield').toggle();
+});
+
+$('.header-nav').hide();
+
+$('#menu').click(function(){
+    $('.header-nav').toggle();
+});
+
+$('#utbildningar-dropdown').hide();
+
+$('#utbildningar-click').click(function(){
+    $('#studenttjänster-dropdown').hide();
+    $('#vägledning-dropdown').hide();
+    $('#kontakt-dropdown').hide();
+
+    $('#utbildningar-dropdown').toggle();
+});
+
+
+$('#studenttjänster-dropdown').hide();
+
+$('#studenttjänster-click').click(function(){
+    $('#vägledning-dropdown').hide();
+    $('#kontakt-dropdown').hide();
+    $('#utbildningar-dropdown').hide();
+
+    $('#studenttjänster-dropdown').toggle();
+});
+
+
+$('#vägledning-dropdown').hide();
+
+$('#vägledning-click').click(function(){
+    $('#kontakt-dropdown').hide();
+    $('#utbildningar-dropdown').hide();
+    $('#studenttjänster-dropdown').hide();
+
+    $('#vägledning-dropdown').toggle();
+});
+
+
+$('#kontakt-dropdown').hide();
+
+$('#kontakt-click').click(function(){
+    $('#utbildningar-dropdown').hide();
+    $('#studenttjänster-dropdown').hide();
+    $('#vägledning-dropdown').hide();
+
+    $('#kontakt-dropdown').toggle();
+});
+
+// Ändrar färg på menyval när användaren klickar på den
+
+$('#utbildningar-click').click(function(){
+
+    $('#studenttjänster-click').css('color', 'grey');
+    $('#vägledning-click').css('color', 'grey');
+    $('#kontakt-click').css('color', 'grey');
+
+
+    var color = $('#utbildningar-click').css('color');
+    if (color == 'rgb(128, 128, 128)' || color == 'grey'){
+        $('#utbildningar-click').css('color', 'white');
+    }
+    if (color == 'rgb(255, 255, 255)' || color == 'white'){
+        $('#utbildningar-click').css('color', 'grey');
+    }
+});
+
+$('#studenttjänster-click').click(function(){
+
+    $('#utbildningar-click').css('color', 'grey');
+    $('#vägledning-click').css('color', 'grey');
+    $('#kontakt-click').css('color', 'grey');
+
+    var color = $('#studenttjänster-click').css('color');
+    if (color == 'rgb(128, 128, 128)' || color == 'grey'){
+        $('#studenttjänster-click').css('color', 'white');
+    }
+    if (color == 'rgb(255, 255, 255)' || color == 'white'){
+        $('#studenttjänster-click').css('color', 'grey');
+    }
+});
+
+$('#vägledning-click').click(function(){
+
+    $('#utbildningar-click').css('color', 'grey');
+    $('#studenttjänster-click').css('color', 'grey');
+    $('#kontakt-click').css('color', 'grey');
+
+    var color = $('#vägledning-click').css('color');
+    if (color == 'rgb(128, 128, 128)' || color == 'grey'){
+        $('#vägledning-click').css('color', 'white');
+    }
+    if (color == 'rgb(255, 255, 255)' || color == 'white'){
+        $('#vägledning-click').css('color', 'grey');
+    }
+});
+
+$('#kontakt-click').click(function(){
+
+    $('#utbildningar-click').css('color', 'grey');
+    $('#vägledning-click').css('color', 'grey');
+    $('#studenttjänster-click').css('color', 'grey');
+
+    var color = $('#kontakt-click').css('color');
+    if (color == 'rgb(128, 128, 128)' || color == 'grey'){
+        $('#kontakt-click').css('color', 'white');
+    }
+    if (color == 'rgb(255, 255, 255)' || color == 'white'){
+        $('#kontakt-click').css('color', 'grey');
+    }
 });
 
 
 
-// Bildspel (Simon)
-let sliderImages = document.querySelectorAll('.slide'),
-    arrowLeft = document.querySelector('#arrow-left'),
-    arrowRight = document.querySelector('#arrow-right'),
-    current = 0;
 
-//clear all images
-function reset(){
-    for (let i=0; i<sliderImages.length; i++){
-        sliderImages[i].style.display = 'none';
+
+// Ändrar färg på sök-ikonen när användaren klickar på den
+
+$('#searchicon').click(function(){
+    var color = $('#searchicon').css('color');
+    if (color == 'rgb(128, 128, 128)' || color == 'grey'){
+        $('#searchicon').css('color', 'white');
     }
-}
-
-//init slider
-function startSlide(){
-    reset();
-    sliderImages[0].style.display = 'block';
-}
-
-// show prev
-function slideLeft(){
-    reset();
-    sliderImages[current - 1].style.display = 'block';
-    current--;
-}
-// show next
-function slideRight(){
-    reset();
-    sliderImages[current +1].style.display = 'block';
-    current++;
-}
-
-// left arrow click
-arrowLeft.addEventListener('click', function(){
-    if (current === 0){
-        current = sliderImages.length;
+    if (color == 'rgb(255, 255, 255)' || color == 'white'){
+        $('#searchicon').css('color', 'grey');
     }
-    slideLeft()
 });
-// right arrow click
-arrowRight.addEventListener('click', function(){
-    if (current === sliderImages.length -1){
-        current = -1;
+
+// Ändrar färg på användar-ikonen när användaren klickar på den
+
+$('#usericon').click(function(){
+    var color = $('#usericon').css('color');
+    if (color == 'rgb(128, 128, 128)' || color == 'grey'){
+        $('#usericon').css('color', 'white');
     }
-    slideRight();
+    if (color == 'rgb(255, 255, 255)' || color == 'white'){
+        $('#usericon').css('color', 'grey');
+    }
 });
-startSlide();
-// Bildspel (Simon) slut
+
+// Ändrar färg på menyvalet utbildningar när användaren klickar
+$('#menuutb').click(function(){
+    var color = $('#menuutb').css('color');
+    if (color == 'rgb(128, 128, 128)' || color == 'grey'){
+        $('#menuutb').css('color', 'white');
+    }
+    if (color == 'rgb(255, 255, 255)' || color == 'white'){
+        $('#menuutb').css('color', 'grey');
+    }
+});
+
+
+
+
+
+
+
+// Desktop
+
+// Döljer och visar dropdown
+$('.desktoputbildningar').hide();
+
+$('#menuutb').click(function(){
+    $('.desktoputbildningar').toggle();
+});
+
+
