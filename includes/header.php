37b7c9727542874e9eecf6f66c60d6c8af9f8945
[madde1] <[maddeswish@gmail.com]>
2018-11-28 16:52:52 +0100

    lägger till icon

--- includes/header.php
+++ includes/header.php
@@ -17,6 +17,15 @@ session_start();
     <link rel="stylesheet" href="../css/reg.css">
     <link href="https://fonts.googleapis.com/css?family=Roboto|Roboto+Slab" rel="stylesheet">
 
+    <!--Icon till sidan -->
+    <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png">
+    <link rel="manifest" href="/favicon/site.webmanifest">
+    <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5">
+    <meta name="msapplication-TileColor" content="#da532c">
+    <meta name="theme-color" content="#ffffff">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Campus Mölndal</title>
 </head>
