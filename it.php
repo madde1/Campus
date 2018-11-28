831034085f1c3ddfe792375f996ca9f59f49bbf4
[madde1] <[maddeswish@gmail.com]>
2018-11-28 10:44:50 +0100

    lägger till sofias saker

---
+++ it.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 87sofrid
+ * Date: 2018-11-21
+ * Time: 10:53
+ */
+?>
+
+
+<body>
+
+<?php
+include 'includes/header.php';
+?>
+
+
+<main>
+    <div class="wrap">
+        <div class="slider">
+            <div class="headerIt">
+                <div class="slide-content">
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="itInfo">
+        <?php
+        /** Inkluderar itInfo delen på sidan. /Sofia */
+        include 'includes/itinfo.php';
+        ?>
+    </section>
+    <section class="AnsokanIt">
+        <div class="ansokanKontaktIt">
+            <h3>Ansökan</h3>
+            <p>
+                Ansök till IT-projektledare med start januari 2019. Ansökan stänger 26 november 2018. Kontakta oss för
+                mer information.
+
+                Alla ansökningar som inkommer behandlas löpande. Är du nyfiken eller inte hör något från oss så hör av
+                dig till oss!
+            </p>
+        </div>
+    </section>
+</main>
+
+<?php
+include "includes/footer.php"
+?>
---
+++ javaStart.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 87sofrid
+ * Date: 2018-11-21
+ * Time: 10:23
+ */
+?>
+
+<?php
+include 'includes/header.php';
+?>
+
+
+<main>
+    <div class="wrap">
+        <div class="slider">
+            <div class="headerBild">
+                <div class="slide-content">
+                </div>
+            </div>
+        </div>
+    </div>
+    <section class="javaInfo">
+        <?php
+        /** Inkluderar JavaInfo delen på sidan. /Sofia */
+        include 'includes/javainfo.php';
+        ?>
+    </section>
+    <section class="AnsokanJava">
+        <div class="ansokanKontaktJava">
+            <h3>Ansökan</h3>
+            <p>
+                Ansökan för start HT2018 är stängd.
+            </p>
+        </div>
+    </section>
+</main>
+
+<?php
+include "includes/footer.php"
+?>
